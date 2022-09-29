29c26109c31902918d0a6685c33fb3dff8af1d5d
dilshodbek <dilshodbekotajonov95@gmail.com>
2022-09-29 15:28:29 +0500

    Init

---
+++ src/main/java/com/example/onlinestoremicroservices/OnlineStoreMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinestoremicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinestoremicroservices/OnlineStoreMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinestoremicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
